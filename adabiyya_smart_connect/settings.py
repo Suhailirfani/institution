fee5b063e98d4eccfb2ae711878e65560a7d68ea
suhail <313suhi313@gmail.com>
2026-04-17 22:59:54 +0530

    Fix ALLOWED_HOSTS for PythonAnywhere

--- adabiyya_smart_connect/settings.py
+++ adabiyya_smart_connect/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.getenv(
 
 DEBUG = os.getenv("DJANGO_DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1,adabiyya.pythonanywhere.com").split(",")
 
 
 # Application definition
